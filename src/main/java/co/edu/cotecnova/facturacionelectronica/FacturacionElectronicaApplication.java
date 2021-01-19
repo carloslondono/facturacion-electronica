be7e4bf0082125aa8a74871a38480e203d51207f
Carlos Londoño <carloslondono.co@gmail.com>
2021-01-18 20:22:20 -0500

    creación del proyecto facturación electrónica

---
+++ src/main/java/co/edu/cotecnova/facturacionelectronica/FacturacionElectronicaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.cotecnova.facturacionelectronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionElectronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionElectronicaApplication.class, args);
+	}
+
+}
